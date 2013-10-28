2981f6c9d4bf133a80819cccb70cd5b6055d9d59
Andy O'Neill <andy@potatoriot.com>
2013-10-28 18:12:54 -0400

    Allow reconfig, improve stats / last result

--- lib/dynosaur.rb
+++ lib/dynosaur.rb
@@ -1,6 +1,10 @@
 require 'dynosaur/scaler_plugin'
 require 'dynosaur/heroku_manager'
 
+require 'pp'
+require 'pry'
+require 'json'
+
 module Dynosaur
     class << self
         DEFAULT_MIN_WEB_DYNOS = 2
@@ -8,7 +12,7 @@ module Dynosaur
         DEFAULT_SCALER_INTERVAL = 5      # seconds between wakeups
         DEFAULT_DOWNSCALE_BLACKOUT = 300 # seconds to wait after change before dropping
 
-        attr_reader :min_web_dynos, :max_web_dynos, :heroku_app_name, :heroku_api_key, :plugins, :current_estimate, :current
+        attr_reader :min_web_dynos, :max_web_dynos, :heroku_app_name, :heroku_api_key, :plugins, :current_estimate, :current, :interval
 
         def initialize(config)
             puts "Running initialize"
@@ -20,6 +24,8 @@ module Dynosaur
             @current = 0
             @desired_state
             @last_change_ts = nil
+            @last_results = {}
+            @server = nil
         end
 
 
@@ -34,7 +40,7 @@ module Dynosaur
                 now = Time.now
 
                 before = @heroku_manager.get_current_dynos
-                @current_estimate, details = get_combined_estimate
+                @current_estimate = get_combined_estimate
                 @desired_state = get_desired_state(before, @current_estimate, now)
 
                 if @desired_state != before
@@ -47,7 +53,12 @@ module Dynosaur
                     @last_change_ts = Time.now
                 end
                 @current = after
-                puts "#{now} [#{before}=>#{after} (#{@current_estimate})] - #{details}"
+                details = ""
+                @last_results.each { |name, result|
+                    details += "#{name}: #{result["value"]}, #{result["estimate"]}; "
+                }
+                puts "#{now} current: #{@current_estimate}; #{before}=>#{after}] - #{details}"
+
                 sleep @interval
                 if @stats
                     output_stats(now, @current_estimate, @desired_state, before, after)
@@ -59,6 +70,10 @@ module Dynosaur
             @stopped = true
         end
 
+        def stop
+            stop_autoscaler
+        end
+
         # start the autoscaler in a new thread
         def start_in_thread
             thread = Thread.new {
@@ -67,16 +82,66 @@ module Dynosaur
             return thread
         end
 
+        # Get status hash (used in dynosaur-rails)
+        def get_status
+            status = {
+                "time" => Time.now.to_i,
+                "current" => @current,
+                "current_estimate" => @current_estimate,
+                "last_changed" => @last_change_ts.to_i,
+                "results" => @last_results
+            }
+            return status
+        end
+
+        # Modify config at runtime
+        def set_config(config)
+            puts "Dynosaur reconfig:"
+            pp  config
+
+            if config.has_key?("scaler")
+                puts "Modifying scaler config"
+                scaler_config = config["scaler"]
+                @min_web_dynos = scaler_config.fetch("min_web_dynos", @min_web_dynos)
+                @max_web_dynos = scaler_config.fetch("max_web_dynos", @max_web_dynos)
+                @interval = scaler_config.fetch("interval", @interval)
+                @blackout = scaler_config.fetch("blackout", @blackout)
+
+                @heroku_api_key = scaler_config.fetch("heroku_api_key", @heroku_api_key)
+                @heroku_app_name = scaler_config.fetch("heroku_app_name", @heroku_app_name)
+            end
+            if config.has_key?("plugins")
+                config["plugins"].each { |plugin_config|
+                    found = nil
+                    @plugins.each { |plugin|
+                        if plugin.name == plugin_config["name"]
+                            puts "Replacing config for #{plugin.name}"
+                            @plugins.delete(plugin)
+                        end
+                    }
+                    if found.nil?
+                        puts "Configuring new plugin"
+                    end
+                    @plugins << config_one_plugin(plugin_config)
+                }
+            end
+        end
+
         def get_combined_estimate
             estimates = []
-            status = ""
+            details = {}
             # Get the estimated dynos from all configured plugins
             @plugins.each { |plugin|
                 value = plugin.get_value
                 estimate = plugin.estimated_dynos  # minor race condition, but only matters for logging
-                status += "[#{plugin.name}] #{value} #{plugin.unit}, #{estimate} dynos;  "
+                details[plugin.name] = {
+                    "estimate" => estimate,
+                    "value" => value,
+                    "unit" => plugin.unit
+                }
                 estimates << estimate
             }
+            @last_results = details
 
             # Combine the estimates and mo
             combined_estimate = estimates.max
@@ -84,7 +149,7 @@ module Dynosaur
             combined_estimate = [@max_web_dynos, combined_estimate].min
             combined_estimate = [@min_web_dynos, combined_estimate].max
 
-            return combined_estimate, status
+            return combined_estimate
         end
 
         # Give the current desired state, taking into account hysteresis i.e. blackout period
@@ -139,30 +204,40 @@ module Dynosaur
         # Take the plugin config and return a bunch of plugin instances
         # No magic, we just compare config['type'] to the plugin class name
         def config_plugins(plugin_config)
-            subclasses = ScalerPlugin.subclasses
-
             @plugins = []
             plugin_config.each { |config|
-                plugin = nil
-                subclasses.each { |klass|
-                    if klass.name == config["type"]
-                        puts "Instantiating #{klass.name} for config '#{config["name"]}'"
-                        plugin = klass.new(config)
-                        break
-                    end
-                }
-                # Error if plugin type was not found
-                if plugin.nil?
-                    raise "Couldn't find plugin type #{config["type"]}"
-                end
-                @plugins << plugin
+                @plugins << config_one_plugin(config)
             }
             return @plugins
         end
 
+        def config_one_plugin(config)
+            subclasses = ScalerPlugin.subclasses
+            plugin = nil
+            subclasses.each { |klass|
+                if klass.name == config["type"]
+                    puts "Instantiating #{klass.name} for config '#{config["name"]}'"
+                    plugin = klass.new(config)
+                    break
+                end
+            }
+            # Error if plugin type was not found
+            if plugin.nil?
+                raise "Couldn't find plugin type #{config["type"]}"
+            end
+            return plugin
+        end
+
         def output_stats(now, combined_estimate, desired_state, before, after)
+            row = "#{now},#{now.to_i},#{combined_estimate},#{desired_state},#{before},#{after}"
+            results = @last_results  # try to minimize race conditions in the iteration
+            results.keys.sort.each { |name|
+                result = results[name]
+                row += ",#{name},#{result["estimate"]},#{result["value"]}"
+            }
+
             open('stats.txt', 'a') { |f|
-                f.puts "#{now},#{now.to_i},#{combined_estimate},#{desired_state},#{before},#{after}"
+                f.puts row
             }
         end
 
