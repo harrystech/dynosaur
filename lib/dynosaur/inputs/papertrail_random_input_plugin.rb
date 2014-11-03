eecac7e352de64d0eeecfb00df7cac752830f0da
Pierre Jambet <pierre.jambet@gmail.com>
2014-11-03 15:12:03 -0500

    Add new random input plugin for redis and papertrail
    Will mainly be useful for dry run tests

---
+++ lib/dynosaur/inputs/papertrail_random_input_plugin.rb
@@ -0,0 +1,40 @@
+
+module Dynosaur
+  module Inputs
+    class PapertrailRandomInputPlugin < AbstractInputPlugin
+
+      def initialize(config)
+        super(config)
+        @max_percentage_threshold = 90.0
+        @unit = "randoms"
+      end
+
+      def retrieve
+        v = SecureRandom.random_number(50000)
+        puts "Generated new random int: #{v}"
+        return v
+      end
+
+      def value_to_resources(value)
+        return suitable_plans(value).first
+      end
+
+      def suitable_plans(value)
+        return plans.select{ |plan|
+          plan["max_log_volume"] * (@max_percentage_threshold / 100) > value
+        }.sort_by{ |plan|
+          plan['max_log_volume']
+        }
+      end
+
+      def plans
+        Dynosaur::Addons.plans_for_addon('papertrail')
+      end
+
+      def self.get_config_template
+        {}
+      end
+
+    end
+  end # Inputs
+end # Dynosaur
