1c12d8de37fff4ab6458a763e6a6093ff2c9bd53
Pierre Jambet <pierre.jambet@gmail.com>
2015-04-23 15:22:25 -0400

    Use addon_id instead of addon_name for the Addon Update Api call
    Even though this is undocumented, using the name as suggested by the doc would
    return a 404:
    https://devcenter.heroku.com/articles/platform-api-reference#add-on

--- lib/dynosaur/heroku_addon_manager.rb
+++ lib/dynosaur/heroku_addon_manager.rb
@@ -18,14 +18,25 @@ module Dynosaur
 
     def set(plan)
       if !@dry_run
-        @heroku_platform_api.addon.update(@app_name, @addon_name, {plan: get_plan_id(plan)})
+        @heroku_platform_api.addon.update(@app_name, addon_id, {plan: get_plan_id(plan)})
       end
       @current_value = plan
     end
 
+    def addon_id
+      return @addon_id ||= current_addon['id']
+    end
+
+    def current_addon
+      if @current_addon.nil?
+        addons = @heroku_platform_api.addon.list(@app_name)
+        @current_addon = addons.find { |addon| addon['addon_service']['name'] == @addon_name }
+      end
+      return @current_addon
+    end
+
     def get_current_plan
-      addons = @heroku_platform_api.addon.list(@app_name)
-      return addons.find { |addon| addon['addon_service']['name'] == @addon_name }['plan']
+      return current_addon['plan']
     end
 
     def get_plan_id(plan)
