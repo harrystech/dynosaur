964c1b8401f260b208586e1788dbbfff6d0b4bec
Andy O'Neill <aoneill@harrys.com>
2015-11-20 15:05:18 -0500

    Set bugsnag release_stage
    - production is default

--- lib/dynosaur/error_handler/bugsnag.rb
+++ lib/dynosaur/error_handler/bugsnag.rb
@@ -5,6 +5,7 @@ module Dynosaur::ErrorHandler
     def initialize(config)
       ::Bugsnag.configure do |bugsnag_config|
         bugsnag_config.api_key = config[:api_key]
+        bugsnag_config.release_stage = config.fetch(:release_stage, "production")
       end
       super
     end
